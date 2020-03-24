cc6b06dd0a46e70800e5b55f6fb82b2d7f953ae2
Michael Marango <mike.marango@gmail.com>
2020-03-24 20:47:28 +0300

    completed authorization and authorization

--- src/resources/movie.py
+++ src/resources/movie.py
@@ -1,7 +1,8 @@
 from flask import Response, request
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_restful import Resource
 
-from src.database.models import Movie
+from src.database.models import Movie, User
 
 
 class MoviesApi(Resource):
@@ -11,23 +12,35 @@ class MoviesApi(Resource):
         return Response(movies, mimetype="application/json", status=200)
 
     @staticmethod
+    @jwt_required
     def post():
+        user_id = get_jwt_identity()
         body = request.get_json()
-        movie = Movie(**body).save()
+        user = User.objects.get(id=user_id)
+        movie = Movie(**body, added_by=user)
+        movie.save()
+        user.update(push__movies=movie)
+        user.save()
         id = movie.id
         return {'id': str(id)}, 200
 
 
 class MovieApi(Resource):
     @staticmethod
+    @jwt_required
     def put(id):
+        user_id = get_jwt_identity()
+        movie = Movie.objects.get(id=id, added_by=user_id)
         body = request.get_json()
-        Movie.objects.get(id=id).update(**body)
+        movie.update(**body)
         return '', 200
 
     @staticmethod
+    @jwt_required
     def delete(id):
-        Movie.objects.get(id=id).delete()
+        user_id = get_jwt_identity()
+        movie = Movie.objects.get(id=id, added_by=user_id)
+        movie.delete()
         return '', 200
 
     @staticmethod
