0bcf57cd75f084fb3396c9e1365cc7e16e56a022
Michael Marango <mike.marango@gmail.com>
2020-03-24 19:23:55 +0300

    login just works

--- src/app.py
+++ src/app.py
@@ -1,15 +1,20 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_restful import Api
+from flask_jwt_extended import JWTManager
 
 from src.database.db import initialize_db
 from src.resources.routes import initialize_routes
 
 APP = Flask(__name__)
 API = Api(APP)
+BCRYPT = Bcrypt(APP)
+JWT = JWTManager(APP)
 
 APP.config['MONGODB_SETTINGS'] = {
     'host': 'mongodb://localhost/movies-ex'
 }
+APP.config.from_envvar('ENV_FILE_LOCATION')
 
 initialize_db(APP)
 initialize_routes(API)
--- src/resources/routes.py
+++ src/resources/routes.py
@@ -1,6 +1,9 @@
+from .auth import SignupApi, LoginApi
 from .movie import MoviesApi, MovieApi
 
 
 def initialize_routes(api):
     api.add_resource(MoviesApi, '/api/movies')
     api.add_resource(MovieApi, '/api/movies/<id>')
+    api.add_resource(SignupApi, '/api/auth/signup')
+    api.add_resource(LoginApi, '/api/auth/login')
